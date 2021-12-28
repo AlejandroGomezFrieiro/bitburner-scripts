d4a86be678228572d5b9e13e933194bef44f105c
Alejandro Gomez Frieiro <alejandrogomezfrieiro@gmail.com>
2021-12-28 19:57:18 +0100

    Added export to all constants

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,10 @@
-const EXCLUDED_SERVERS = ["home", "pserv", "darkweb"];
+export const EXCLUDED_SERVERS = ["home", "pserv", "darkweb"];
 
-const FILES_TO_DISTRIBUTE = ["worker.js", "weaken.js", "grow.js", "hack.js"];
+export const FILES_TO_DISTRIBUTE = ["worker.js", "weaken.js", "grow.js", "hack.js"];
 
-const NEXT_TARGET_PORT = 1;
+export const NEXT_TARGET_PORT = 1;
 
-const HACKING_SYNC_CONSTANT = 200;
+export const HACKING_SYNC_CONSTANT = 200;
 
-const SERVER_FORTIFY_AMOUNT = 0.002; // Amount by which server's security increases when its hacked/grown
-const SERVER_WEAKEN_AMOUNT = 0.05; // Amount by which server's security decreases when weakened
+export const SERVER_FORTIFY_AMOUNT = 0.002; // Amount by which server's security increases when its hacked/grown
+export const SERVER_WEAKEN_AMOUNT = 0.05; // Amount by which server's security decreases when weakened

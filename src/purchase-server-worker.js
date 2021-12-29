b7bf80cce1007a8f05d73f1d46ca877562568a2a
Alejandro Gomez Frieiro <alejandrogomezfrieiro@gmail.com>
2021-12-29 15:45:00 +0100

    Added script to purchase servers

---
+++ src/purchase-server-worker.js
@@ -0,0 +1,45 @@
+/** @param {import(".").NS } ns **/
+
+export async function main(ns) {
+
+    const maxRam = ns.getPurchasedServerMaxRam();
+    const maxServers = ns.getPurchasedServerLimit();
+    const serverName = "pserv";
+    // Const max number of servers
+    while (true) {
+        // let myMoney = ns.getServerMoneyAvailable("home");
+        const purchasedServers = ns.getPurchasedServers();
+        let counter = 0
+        while (purchasedServers < maxServers) {
+            if (ns.getServerMoneyAvailable("home") > ns.getPurchasedServerCost(2)) {
+                ns.purchaseServer(`${serverName}-${counter}`, 2);
+                ++counter;
+            }
+        }
+
+        if (purchasedServers.length > 0) {
+            for (let server of purchasedServers) {
+                if (canUpgradeServer(ns, server, ns.getServerMoneyAvailable("home"))){
+                    upgradeServer(ns, server);
+                }
+            }
+        }
+        await ns.sleep(200);
+    }
+}
+
+/** @param {import(".").NS } ns */
+export function canUpgradeServer(ns, server, availableMoney) {
+    const targetRAMPrice = ns.getPurchasedServerCost(2 * ns.getServerMaxRam(server));
+    if (availableMoney > targetRAMPrice) {
+        return true;
+    }
+    return false;
+
+}
+
+/** @param {import(".").NS } ns */
+export function upgradeServer(ns, server) {
+    ns.deleteServer(server);
+    ns.purchaseServer(server, 2 * ns.getServerMaxRam(server));
+}
